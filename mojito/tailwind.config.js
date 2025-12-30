218d194ed51fad9b57482937bd704a9be05dfcbb
Ashima <ashiashimap@gmail.com>
2025-12-30 11:14:13 +0400

    initial commit

---
+++ mojito/tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#e7d393",
+        "white-100": "#efefef",
+      },
+      fontFamily: {
+        sans: ["Mona Sans", "sans-serif"],
+        "modern-negra": ["Modern Negra", "sans-serif"],
+        serif: ["DM Serif Text", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
